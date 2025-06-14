576ccb07938a526a9dc6b271de25bef2485f7932
devilal <devilalchoudhary67@gmail.com>
2025-06-14 21:25:57 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
+import express from "express";
 
 dotenv.config({
     path : './env'
 })
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed", err);
+})
---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error{
+    constructor(statusCode,
+        massage = "something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(massage)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = this.message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
